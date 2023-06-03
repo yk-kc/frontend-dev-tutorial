46b4481687fee67559c76a7c167c0da830f5184b
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-06-03 19:54:24 +0900

    変数の宣言方法とデータ型

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,15 @@
+// var, let, const
+
+let name = 'Tom'
+name = 'Tim'
+console.log('Hello ' + name)
+
+// Number, String, Boolean, Undefined, Null, Symbol
+
+let variable = 'str'
+variable = 12
+variable = false
+variable = undefined
+
+// 変数名に格納した値のデータ型を確認できる
+console.log(typeof variable)
--- 04_JavaScriptの基礎を固める/060_ループの記述方法/start/main.js
+++ 04_JavaScriptの基礎を固める/060_ループの記述方法/start/main.js
@@ -1,5 +1,5 @@
-const arry = [1, 2, 3, 4, 5]
+const arry = [1, 2, 3, 4, 5, 6]
 
-for (let i = 0; i < 5; i = i + 1) {
-  console.log(i)
+for (let i = 0; i < arry.length; i++) {
+  console.log(arry[i])
 }
