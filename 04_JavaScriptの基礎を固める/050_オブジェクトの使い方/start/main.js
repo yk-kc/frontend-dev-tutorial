c88acf5fecc7660fdcf19eb5de9801c3fcabbe81
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-06-05 02:53:27 +0900

    オブジェクトを使ってデータを管理

--- 04_JavaScriptの基礎を固める/050_オブジェクトの使い方/start/main.js
+++ 04_JavaScriptの基礎を固める/050_オブジェクトの使い方/start/main.js
@@ -6,9 +6,12 @@ const person = {
     sports: 'soccer',
     music: 'piano',
   },
-  getFullName: func,
+  getFullName: function () {
+    console.log(this.name[0] + this.name[1])
+  },
 }
 
 const ageKey = 'age'
 person[ageKey] = 31
 console.log(person.age)
+person.getFullName()
