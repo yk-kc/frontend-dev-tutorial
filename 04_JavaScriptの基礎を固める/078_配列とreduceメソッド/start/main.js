72f0dc50b9a2e8354cc04fa34f57b4e0aead64b3
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-06-09 05:02:47 +0900

    reduceメソッドの使い方

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,25 @@
-const arry = [1,2,3,4,5];
+const arry = [1, 2, 3, 4, 5]
+
+// reduceメソッド
+arry.reduce(
+  // reduceメソッドの第一引数
+  function (accu, cur) {
+    console.log(accu, cur)
+    // return accu * cur
+  },
+  // reduceメソッドの第二引数
+  10
+)
+
+const str = 'animation'
+const strArry = str.split('')
+
+console.log(strArry)
+
+const result = strArry.reduce((accu, cur) => {
+  // テンプレートリテラルを使うとより簡潔に記述できる
+  return `${accu}<${cur}>`
+  // return accu + '<' + cur + '>'
+}, '')
+
+console.log(result)
