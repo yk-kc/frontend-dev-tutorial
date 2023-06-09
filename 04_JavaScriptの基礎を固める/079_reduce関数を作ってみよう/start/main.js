6959d7840ed9b215e1ff4fbf8eb2ee0557b1db48
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-06-09 19:17:40 +0900

    reduce関数を作ってみよう

--- 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
@@ -1,9 +1,20 @@
-const str = 'animation';
-const strArry = str.split('');
+const str = 'animation'
+const strArry = str.split('')
 
 function tag(accu, curr) {
-    return `${accu}<${curr}>`;
+  console.log(accu, curr)
+  return `${accu}<${curr}>`
 }
 
-const result = strArry.reduce(tag, "");
-console.log(result);
+function reduce(arry, callback, defaultValue) {
+  let accu = defaultValue
+  for (let i = 0; i < arry.length; i++) {
+    let curr = arry[i]
+    accu = callback(accu, curr)
+  }
+
+  return accu
+}
+
+const result = reduce(strArry, tag, '')
+console.log(result)
