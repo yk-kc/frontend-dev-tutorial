7e120df6b8ed852aa7e0962f61e390b231aa61d5
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-06-10 15:58:59 +0900

    DOM

--- 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
+++ 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
@@ -1,10 +1,11 @@
-const dcl = document.querySelector('.dcl');
-const load = document.querySelector('.load');
+const dcl = document.querySelector('.dcl')
+const load = document.querySelector('.load')
 
-document.addEventListener("DOMContentLoaded", function () {
-    dcl.classList.add('done');
-});
+document.addEventListener('DOMContentLoaded', function () {
+  dcl.classList.add('done')
+})
 
-window.addEventListener("load", function () {
-    load.classList.add('done');
-});
+// document.は使えない
+window.addEventListener('load', function () {
+  load.classList.add('done')
+})
