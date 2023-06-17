474054f54379c4ce22e718200e7c0769a28d7268
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-06-18 03:15:10 +0900

    DOM操作練習

---
+++ 04_JavaScriptの基礎を固める/110_DOM操作練習_文字分割/start/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const el = document.querySelector('.animate-title')
+  const str = el.innerHTML.trim()
+  let concatStr = ''
+
+  for (let c of str) {
+    concatStr = concatStr + `<span class="char">${c}</span> `
+  }
+  el.innerHTML = concatStr
+  console.log(concatStr)
+})
