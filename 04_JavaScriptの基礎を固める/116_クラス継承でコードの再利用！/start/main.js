062186c4e31a2453d0fc95d751d79144e3ab58e4
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-06-23 19:35:49 +0900

    classを継承

--- 04_JavaScriptの基礎を固める/116_クラス継承でコードの再利用！/start/main.js
+++ 04_JavaScriptの基礎を固める/116_クラス継承でコードの再利用！/start/main.js
@@ -1,26 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const btn = document.querySelector('#btn');
-    const ta = new TextAnimation('.animate-title');
-    const ta2 = new TextAnimation('.animate-title-2');
-    ta.animate();
-    ta2.animate();
-    btn.addEventListener('click', ta.animate.bind(ta));
-});
-
+  const ta = new TweenTextAnimation('.tween-animate-title')
+  ta.animate()
+})
 
 class TextAnimation {
-    constructor(el) {
-        this.el = document.querySelector(el);
-        this.chars = this.el.innerHTML.trim().split("");
-        this.el.innerHTML = this._splitText();
-    }
-    _splitText() {
-        return this.chars.reduce((acc, curr) => {
-            curr = curr.replace(/\s+/, '&nbsp;');
-            return `${acc}<span class="char">${curr}</span>`;
-        }, "");
-    }
-    animate() {
-        this.el.classList.toggle('inview');
-    }
+  constructor(el) {
+    this.DOM = {}
+    this.DOM.el = document.querySelector(el)
+    // this.DOM.el = document.querySelector(el)
+    this.chars = this.DOM.el.innerHTML.trim().split('')
+    this.DOM.el.innerHTML = this._splitText()
+  }
+  _splitText() {
+    return this.chars.reduce((acc, curr) => {
+      curr = curr.replace(/\s+/, '&nbsp;')
+      return `${acc}<span class="char">${curr}</span>`
+    }, '')
+  }
+  animate() {
+    this.DOM.el.classList.toggle('inview')
+  }
+}
+
+class TweenTextAnimation extends TextAnimation {
+  constructor(el) {
+    super(el)
+    this.DOM.chars = this.DOM.el.querySelectorAll('.char')
+  }
+
+  animate() {
+    this.DOM.el.classList.add('inview')
+    this.DOM.chars.forEach((c, i) => {
+      TweenMax.to(c, 0.6, {
+        ease: Back.easeOut,
+        delay: i * 0.05,
+        startAt: { y: '-50%', opacity: 0 },
+        y: '0%',
+        opacity: 1,
+      })
+    })
+  }
 }
